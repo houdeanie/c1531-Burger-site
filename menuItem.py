0f241fda9a549403cfdbc8dba8a0d33c002c9b6c
Stephen Wang <stephen.wang.unsw@gmail.com>
2019-04-05 20:10:42 +1100

    combined menuItem.py and menuItemSubClasses.py

--- menuItem.py
+++ menuItem.py
@@ -28,3 +28,39 @@ class MenuItem:
 		
 	def set_type(self, type):
 		self._type = type
+		
+class Main(MenuItem):
+	def __init__(self, name, price, quantity, type):
+		super().__init__(name, price, quantity, type)
+		self._ingredients = []
+		
+	def calc_price(self):
+		total = 0.00
+		for x in self._ingredients:
+			total = total + x.get_price()
+		return total
+		
+	def get_ingredients(self):
+		return self._ingredients
+		
+	def add_ingredient(self, ingredient):
+		self._ingredients.insert(0, ingredient)
+		
+class Ingredient(MenuItem):
+	def __init__(self, name, price, quantity, type):
+		super().__init__(name, price, quantity, type)
+		
+class MeasuredItem(MenuItem):
+	def __init__(self, name, price, quantity, type, serving_size):
+		super().__init__(name, price, quantity, type)
+		self._serving_size = serving_size
+		
+	def get_serving_size(self):
+		return self._serving_size
+		
+	def set_serving_size(self, size):
+		self._serving_size = size
+	
+	
+	
+		
