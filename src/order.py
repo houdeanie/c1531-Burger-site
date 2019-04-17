049beafaf83d113e6f88d31ed107592ffb93385f
Stephen Wang <stephen.wang.unsw@gmail.com>
2019-04-17 10:54:25 +1000

    implemented property decorator for order attributes

--- src/order.py
+++ src/order.py
@@ -1,63 +1,55 @@
+from menuItem import MenuItem
+
 #class to represent a customer order
 class Order:
 	def __init__(self):
 		self._id = -1
 		self._status = "ordering"
 		self._items = []
-		self._net_price = float(-1)
+		self._net_price = 0
+		
 	#classes to items in the order
-	def add_item(self, item):
-		self._items.insert(0, item)
-		self.calc_price()
+	def add_item(self, item, price):
+		self._items.append(item)
+		self._net_price += price
 		return
 	
-	def remove_item(self, item):
-		self.calc_price()
+	def remove_item(self, item, price):
+		if not item in self._items:
+			return False
 		index = self._items.index(item)
 		self._items.pop(index)
-		return
-	
-	#class to calculate total price of order
-	def calc_price(self):
-		total = 0.00
-		for x in self._items:
-			total = total + x.get_price()
-		self._net_price = total
-		return
+		self._net_price -= price
+		return True
 		
 	#getter and setter methods	
-	def get_id(self):
+	@property
+	def id(self):
 		return self._id
-		
-	def set_id(self, id):
-		self._id = id
-		
-	def get_status(self):
+	
+	@id.setter	
+	def id(self, value):
+		self._id = value
+	
+	@property	
+	def status(self):
 		return self._status
 		
-	def set_status(self, status):
+	@status.setter	
+	def status(self, status):
 		self._status = status
-		
-	def get_items(self):
-		return self._items
-		
-	def get_net_price(self):
-		return self._net_price	
 	
-	#function to print items in the order
-	def show_items(self):
-		if(len(self._items) == 0):
-			return "none"
-		else:
-			items = []
-			for x in self._items:
-				items.append(x.get_quantity())
-				items.append(x.get_name())
-		return items
+	@property	
+	def items(self):
+		return self._items
+	
+	@property	
+	def net_price(self):
+		return self._net_price	
 	
 	#modified print output
 	def __str__(self):
-		return("id: {0}, status: {1}, items: {2}, net_price: {3}".format(self._id, self._status, self.show_items(), self._net_price))		 
+		return str("id: {0}, status: {1}, items: {2}, net_price: ${3}".format(self._id, self._status, self._items, self._net_price))		 
 
 		
 	
