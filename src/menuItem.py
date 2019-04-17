86c52a09d2cbd63cdc822e8ff8fc42536be2b255
Stephen Wang <stephen.wang.unsw@gmail.com>
2019-04-17 10:51:01 +1000

    implemented property syntax for attributes. changed attribute type to food_type to avoid masking python keyword type. deleted subclass ingredient as it is not needed. added class BaseItem to track the total amount of measured items e.g. nuggets, fries etc...

--- src/menuItem.py
+++ src/menuItem.py
@@ -1,57 +1,63 @@
-#parent class for all food and drink items
+#parent class for all items offered by GourmetBurgers
 class MenuItem:
-	def __init__(self, name, price, quantity, type):
+	def __init__(self, name, price, stock_quantity, food_type):
 		self._name = name
 		self._price = price
-		self._quantity = quantity
-		self._type = type
+		self._stock_quantity = stock_quantity
+		self._food_type = food_type
 
 	#getter and setter functions
-	def get_name(self):
+	@property
+	def name(self):
 		return self._name
 		
-	def set_name(self, name):
+	@name.setter		
+	def name(self, name):
 		self._name = name
 		
-	def get_price(self):
+	@property		
+	def price(self):
 		return self._price
-	
-	def set_price(self, price):
+		
+	@price.setter		
+	def price(self, price):
 		self._price = price
+
+	@property		
+	def stock_quantity(self):
+		return self._stock_quantity
+
+	@stock_quantity.setter		
+	def stock_quantity(self, stock_quantity):
+		self._stock_quantity = stock_quantity
+
+	@property		
+	def food_type(self):
+		return self._food_type
 		
-	def get_quantity(self):
-		return self._quantity
+	@food_type.setter		
+	def food_type(self, food_type):
+		self._food_type = food_type
 		
-	def set_quantity(self, quantity):
-		self._quantity = quantity
+	#modified print output	
+	def __str__(self):
+		return "name: {0}, price: ${1}, stock_quantity: {2}, food_type: {3}".format(self._name, self._price, self._stock_quantity, self._food_type)
 		
-	def get_type(self):
-		return self._type
-		
-	def set_type(self, type):
-		self._type = type
-		
-#subclass to represent mains		
+#subclass for all mains 
 class Main(MenuItem):
-	def __init__(self, name, price, quantity, type):
-		super().__init__(name, price, quantity, type)
-		self._ingredients = []
-		
-	#function to calculate the total price of a main	
-	def calc_price(self):
-		total = 0.00
-		for x in self._ingredients:
-			total = total + x.get_price() * x.get_quantity()
-		self._price = total
-		return 
-		
-	def get_ingredients(self):
-		return self._ingredients
-	
+	def __init__(self, name, price, stock_quantity, food_type, ingredients):
+		super().__init__(name, price, stock_quantity, food_type)
+		self._ingredients = ingredients
+
+	#getter functions
+	@property	
+	def ingredients(self):
+		return self._ingredients[::]
+		 
 	#function to add ingredients 	
 	def add_ingredient(self, ingredient):
-		self._ingredients.insert(0, ingredient)
-		self.calc_price()
+		self._ingredients.append(ingredient)
+		self._price += ingredient.price
 		return
 	
 	#function to show ingredients
@@ -61,73 +67,56 @@ class Main(MenuItem):
 		else:
 			ingredients = []
 			for x in self._ingredients:
-				ingredients.append(x.get_quantity())
+				ingredients.append(x.get_stock_quantity())
 				ingredients.append(x.get_name())
 
 		return ingredients
 		
 	#modified print output	
 	def __str__(self):
-		return "name: {0}, price: {1}, quantity: {2}, type: {3}, ingredients: {4}".format(self._name, self._price, self._quantity, self._type, self.show_ingredients())
+		return "name: {0}, price: ${1}, stock_quantity: {2}, food_type: {3}, ingredients: {4}".format(self._name, self._price, self._stock_quantity, self._food_type, self._ingredients)
 		
-		
-#subclass to represent ingredients that can be added to mains		
-class Ingredient(MenuItem):
-	def __init__(self, name, price, quantity, type):
-		super().__init__(name, price, quantity, type)
-		
-	#modified print output	
-	def __str__(self):
-		return "name: {0}, price: {1}, quantity: {2}, type: {3}".format(self._name, self._price, self._quantity, self._type)
-		
-#subclass of items that come in quantities more than 1 - e.g. drinks, nuggets 
+#subclass for items that come in quantities != 1 and have different sizes e.g. (fries, nuggets, orange juice etc)
 class MeasuredItem(MenuItem):
-	def __init__(self, name, price, quantity, type, serving_size):
-		super().__init__(name, price, quantity, type)
+	def __init__(self, name, price, stock_quantity, food_type, serving_size, base_item):
+		super().__init__(name, price, stock_quantity, food_type)
 		self._serving_size = serving_size
+		self._base_item = base_item
 		
 	#getter and setter functions
-	def get_serving_size(self):
+	@property
+	def serving_size(self):
 		return self._serving_size
-		
-	def set_serving_size(self, size):
+
+	@serving_size.setter	
+	def serving_size(self, size):
 		self._serving_size = size
+		
+	@property
+	def base_item(self):
+		return self._base_item
+		
+	@base_item.setter
+	def base_item(self, quantity):
+		self._base_item = base_item			
 		
 	#modified print output	
 	def __str__(self):
-		return "name: {0}, price: {1}, quantity: {2}, type: {3}, serving_size: {4}".format(self._name, self._price, self._quantity, self._type, self._serving_size)
+		return "name: {0}, price: {1}, stock_quantity: {2}, food_type: {3}, serving_size: {4}, pile_quantity: {5}".format(self._name, self._price, self._stock_quantity, self._food_type, self._serving_size, self._pile_quantity)
 		
-class Nugget(MeasuredItem):
-	_total_nuggets = 10000
-	def __init__(self, name, price, quantity, type, serving_size):
-		super().__init__(name, price, quantity, type, serving_size)
 		
-	def get_total_nuggets(self):
-		return self._total_nuggets
+#subclass for items that are used to keep track of measured items e.g. total nuggets
+class BaseItem(MenuItem):
+	def __init__(self, name, price, stock_quantity, food_type, related_items):
+		super().__init__(name, price, stock_quantity, food_type)
+		self._related_items = related_items
+
+	@property
+	def related_items(self):
+		return self._related_items[::]
 		
-	def set_total_nuggets(self, total):
-		self._total_nuggets = total
-			
-class Fries(MeasuredItem):
-	_total_fries = 10000
-	def __init__(self, name, price, quantity, type, serving_size):
-		super().__init__(name, price, quantity, type, serving_size)
-		
-	def get_total_fries(self):
-		return self._total_fries
-		
-	def set_total_fries(self, total):
-		self._total_fries = total
-		
-class OrangeJuice(MeasuredItem):
-	_total_orange_juice = 10000
-	def __init__(self, name, price, quantity, type, serving_size):
-		super().__init__(name, price, quantity, type, serving_size)
-		
-	def get_total_orange_juice(self):
-		return self._total_orange_juice
-		
-	def set_total_orange_juice(self, total):
-		self._total_orange_juice = total
+	@related_items.setter
+	def related_items(self, related_item):
+		self._related_item = related_item
 
 		
