8fee2561e7fa654cb067c01c0c6a1de037a84cee
Stephen Wang <stephen.wang.unsw@gmail.com>
2019-04-17 14:07:13 +1000

    main.py now properly initialises the system. prints out all menu items after initialising system

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from gourmetBurgerSystem import GourmetBurgerSystem
-
-system = GourmetBurgerSystem()
+from menu_creator import *
+system = GourmetBurgerSystem(create_menu())
 
 items = system.get_menu_items()
 for each in sorted(items.keys()):
 	menu_item = items[each]
-    print("name:",menu_item._name)
-    print("quantity:",menu_item._quantity)
-    print("price:",menu_item._price)	
+	print("name: {0}, quantity: {1}, price: ${2}".format(menu_item._name, menu_item._stock_quantity, menu_item._price))
+
