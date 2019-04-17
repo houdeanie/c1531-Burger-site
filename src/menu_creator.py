8b225b1998ea0deec2877bcdd8601c9f673c10c0
Stephen Wang <stephen.wang.unsw@gmail.com>
2019-04-17 14:04:34 +1000

    calls set_quantity() from menu.py properly now

--- src/menu_creator.py
+++ src/menu_creator.py
@@ -26,33 +26,32 @@ def create_menu():
 	
 	#add sides
 	menu.add_base_item("nugget", 0, 1000, "side", ["small nuggets", "medium nuggets", "large nuggets"])
-	set_quantity("nugget")
 	menu.add_measured_item("small nuggets", 3.0, -1, "side", 3, "nuggets") 
 	menu.add_measured_item("medium nuggets", 5.0, -1, "side", 6, "nuggets")
 	menu.add_measured_item("large nuggets", 7.0, -1, "side", 10, "nuggets") 
+	menu.set_quantity("nugget")
 	menu.add_base_item("fries", 0, 6000, "side", ["small fries", "medium fries", "large fries"])
-	set_quantity("fries")
 	menu.add_measured_item("small fries", 2.5, -1, "side", 75, "fries")
 	menu.add_measured_item("medium fries", 3.5, -1, "side", 150, "fries")
 	menu.add_measured_item("large fries", 4.5, -1, "side", 250, "fries")
+	menu.set_quantity("fries")
 			
 	#add drinks 
 	menu.add_menu_item("water", 3.0, 1000, "drink") 
 	menu.add_base_item("orange juice", 0, 10000, "drink", ["small orange juice", "medium orange juice",  "large orange juice"])
-	set_quantity("orange juice")
 	menu.add_measured_item("small orange juice", 2.5, -1, "drink", 250, "orange juice") 
 	menu.add_measured_item("medium orange juice", 3.5,  -1, "drink", 450, "orange juice")
 	menu.add_measured_item("large orange juice", 4.5, -1, "drink", 650, "orange juice")
+	menu.set_quantity("orange juice")
 	
 	#add sundaes
 	menu.add_base_item("sundae", 0, 10000, "dessert", ["small chocolate sundae", "medium chocolate sundae", "large chocolate sundae", "small strawberry sundae", "medium strawberry sundae", "large strawberry sundae"])
-	set_quantity("sundae")
 	menu.add_measured_item("small chocolate sundae", 3.5, -1, "drink", 250, "sundae") 
 	menu.add_measured_item("medium chocolate sundae", 4.5, -1, "drink", 450, "sundae") 
-	menu.add_measured_item("large chocolate sundae", 5.5, -1, "drink", 650, "sundae") 
-	
+	menu.add_measured_item("large chocolate sundae", 5.5, -1, "drink", 650, "sundae") 	
 	menu.add_measured_item("small strawberry sundae", 3.5, -1, "drink", 250, "sundae") 	
 	menu.add_measured_item("medium strawberry sundae", 4.5, -1, "drink", 450, "sundae")
-	menu.add_measured_item("large strawberry sundae", 5.5, -1, "drink", 650, "sundae") 
+	menu.add_measured_item("large strawberry sundae", 5.5, -1, "drink", 650, "sundae") 	
+	menu.set_quantity("sundae")
 									
 	return menu
