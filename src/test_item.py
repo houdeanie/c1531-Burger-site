14001dfbac90e8837698d432d439de90a88cfe84
Stephen Wang <stephen.wang.unsw@gmail.com>
2019-04-18 22:33:30 +1000

    deleted test_item and test_menu

--- src/test_item.py
+++
@@ -1,9 +0,0 @@
-from menuItem import*
-
-class TestMenuItem():
-    def setup_method(self):
-		self.burger1 = Main("custom burger", 0, 1000,)
-		self.burger2 = 
-
-    def test_add_ingredient(self):
-    	
