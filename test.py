4f3cdace6bfcefb0b229417de4999d2ea2b0f25d
Dean <dean.hou98@hotmail.com>
2019-04-22 23:48:23 +1000

    added tests for getting inveotry items

--- test.py
+++ test.py
@@ -4,38 +4,18 @@ from src.gourmetBurgerSystem import GourmetBurgerSystem
 from src.order import Order
 from src.menu_creator import *
 from src.item import *
-from src.gourmetBurgerSystem import GourmetBurgerSystem
-from src.order import Order
 
-'''
-	menu.add_menu_item("sesame bun", 1, 1000, "ingredient", "burger")
-	menu.add_menu_item("muffin bun", 1.50, 1000, "ingredient", "burger") 
-	menu.add_menu_item("beef patty", 1.5, 1000, "ingredient", "burger")
-	menu.add_menu_item("veg patty", 1, 1000, "ingredient", "burger") 
-	menu.add_menu_item("white wrap", 1.00, 1000, "ingredient", "wrap") 
-	menu.add_menu_item("wholemeal wrap", 1.00, 1000, "ingredient", "wrap") 
-	menu.add_menu_item("tuna filling", 1.5, 1000, "ingredient", "wrap") 
-	menu.add_menu_item("veg filling", 1.5, 1000, "ingredient", "wrap") 
-	menu.add_menu_item("tomato", 0.5, 1000, "ingredient", "burger, wrap") 
-	menu.add_menu_item("lettuce", 0.5, 1000, "ingredient", "burger, wrap")
-	menu.add_menu_item("swiss cheese", 0.5, 1000, "ingredient", "burger, wrap") 
-	menu.add_menu_item("cheddar cheese", 0.5, 1000, "ingredient", "burger, wrap") 
-	menu.add_menu_item("tomato sauce", 0.5, 1000, "ingredient", "burger, wrap")
-	menu.add_menu_item("ians pecial sauce", 2, 1000, "ingredient", "burger, wrap") 
-'''
+
 # testing creating a simple order of a burger
 system = GourmetBurgerSystem(create_menu())
 print(system.display_inventory)
+ingredients = system.display_inventory.get_measured_item('side')
 
-order = system.new_order()
+ingredient = system.display_item('lettuce')
 
+print(ingredients)
+print(len(ingredients))
+print(ingredient)
 
-burger = system.new_main_order("Burger")
-burger.add_ingredient("sesame bun", system.display_inventory.get_price("sesame bun"))
-
-order.add_item(burger, system.display_inventory.get_price("sesame bun"))
-
-order.add_item
-print(order)
 
 
--- test_inventory.py
+++ test_inventory.py
@@ -6,11 +6,24 @@ class TestCurrentOrders():
 	def setup_method(self):
 		self.system = GourmetBurgerSystem(create_menu())
       
-
-	# testing order refill functions
-	def test_refill_inventory(self):
-		menu = self.system.dsiplay_inventory
-		
-
 	# test for seeing the inventory
 	def test_view_inventory(self):
+		ingredients = self.system.display_inventory.get_ingredients()
+		sides = self.system.display_inventory.get_measured_item('side')
+		drinks = self.system.display_inventory.get_measured_item('drink')
+		desserts = self.system.display_inventory.get_measured_item('dessert')
+
+		assert(len(ingredients) == 14)
+		assert(len(sides) == 6)
+		assert(len(drinks) == 4)
+		assert(len(desserts) == 6)
+		
+	# testing order refill functions
+	def test_refill_inventory(self):
+		refill = 100
+		menu = self.system.dsiplay_inventory
+
+		
+		
+
+	
